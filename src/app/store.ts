748fcd1759bc6133e218f3cf9187860f6d914d5b
josh-mo <josh.mo@zendesk.com>
2022-03-21 09:14:36 +1100

    chore: prettier

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,9 +3,9 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 
 export function makeStore() {
-  return configureStore({
-    reducer: { counter: counterReducer },
-  })
+    return configureStore({
+        reducer: { counter: counterReducer },
+    })
 }
 
 const store = makeStore()
@@ -15,10 +15,10 @@ export type AppState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  AppState,
-  unknown,
-  Action<string>
+    ReturnType,
+    AppState,
+    unknown,
+    Action<string>
 >
 
 export default store
